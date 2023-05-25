acea70d82f9557bda147f2759e6fa61a1498f54f
psakiev <psakiev@sandia.gov>
2023-05-24 19:30:23 -0600

    Get kokkos tests passing for real on cpu

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,6 +1,17 @@
 #include "gtest/gtest.h"
 
+#if defined(USE_KOKKOS)
+#include "Kokkos_Core.hpp"
+#define INIT_ARG(X, Y) Kokkos::initialize(X, Y)
+#define END_ARG Kokkos::finalize()
+#else
+#define INIT_ARG(X, Y) [](int& /*unused*/, char** /*unused*/){}
+#define END_ARG void()
+#endif
 int main(int argc, char **argv) {
+  INIT_ARG(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  auto result = RUN_ALL_TESTS();
+  END_ARG;
+  return result;
 }
